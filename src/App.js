683d9d28f882d068fd999a3ed1759c01a92b5315
Juanfunes19 <juan.funes1996@gmail.com>
2022-03-11 18:46:56 -0300

    Primer Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Nav from "./componets/Nav"
+// import Header from "./componets/Header"
+// import Cards from "./componets/Cards"
+// import Footer from "./componets/Footer"
+
+// import Img1 from "./Img/bart"
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Nav />
+    </>
   );
 }
 
---
+++ src/componets/Nav/Nav.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import logo from "../../Img/header-simpsons.gif"
+import "./Nav.css";
+
+const Nav = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <div className="container-fluid">
+    <img src={logo} alt="Logo" className='logo'/> 
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a className="nav-link" aria-current="page" href="#">Inicio</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Blog</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link">Contactos</a>
+        </li>
+      </ul>
+      <form className="d-flex">
+        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+        <button className="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+  )
+}
+
+export default Nav
